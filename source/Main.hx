175ecfeafc243e830103b8f26abe3a27c2515469
kalelplar12 <80426824+kalelplar12@users.noreply.github.com>
2022-08-10 21:45:29 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -88,7 +88,7 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		fpsVar = new FPS(10, 3, 0xFFFFFF);
+		fpsVar = new FPS(11, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
